1cc74fad0bace803aea9dfd6e703472a13c8abca
MikeM97 <mmastrandrea97@aim.com>
2021-09-17 05:07:35 -0400

    1

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirection = 0.1f;
+
+    public float secondsBetweenAppleDrop = 1f;
+
+    void Start()
+    {
+        // Dropping apples every second
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+
+        else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1;
+        }
+    }
+}
